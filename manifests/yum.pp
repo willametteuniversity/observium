9cb424c416505b30ce0a41377b9e6e652e5d05ad
Casey Feskens <cfeskens@willamette.edu>
2022-12-10 19:16:24 -0800

    remove dependency on remi repo

--- manifests/yum.pp
+++ manifests/yum.pp
@@ -31,16 +31,9 @@ class observium::yum {
       $observium::repos.each | String $reponame, Hash $repoinfo | {
         yumrepo { $reponame:
           *      => $repoinfo,
-          before => Exec['Set remi-7.2 as default php provider'],
         }
       }
 
-      # Set remi-7.2 module as default php provider RHEL 8 only
-      exec { 'Set remi-7.2 as default php provider':
-        command => '/bin/dnf module reset php | /bin/dnf module -y install php:remi-7.2',
-        unless  => '/bin/dnf module list php | grep "remi-7.2 \\[e\\]"',
-      }
-    }
       default: { fail('Unsupported operating system, bailing out!!') }
     }
   }
